109080dfdd8e16b9a3cd2fdc5d00394f63337e77
Douglas Willian Dias <douglas.wdias@hotmail.com>
2024-01-26 22:19:42 -0300

    add Dapper package and create Category.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,30 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using BaltaDataAccess.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
 const string connectionString = "server=localhost\\sqlexpress;database=balta;trusted_connection=True;TrustServerCertificate=True";
+
+// using (var connection = new SqlConnection(connectionString))
+// {
+//   connection.Open();
+//   using(var command = new SqlCommand())
+//   {
+//     command.Connection = connection;
+//     command.CommandType = System.Data.CommandType.Text;
+//     command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+//     var reader = command.ExecuteReader();
+//     while(reader.Read())
+//     {
+//       Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+//     }
+//   }
+// }
 
 using (var connection = new SqlConnection(connectionString))
 {
-  connection.Open();
-  using(var command = new SqlCommand())
+  var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+  foreach (var category in categories)
   {
-    command.Connection = connection;
-    command.CommandType = System.Data.CommandType.Text;
-    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
-
-    var reader = command.ExecuteReader();
-    while(reader.Read())
-    {
-      Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-    }
+    Console.WriteLine($"{category.Id} - {category.Title}");
   }
 }
